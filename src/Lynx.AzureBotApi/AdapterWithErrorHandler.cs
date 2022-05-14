bf86222f930bb639d7b3c713f9f6a51ad14dca3b
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2022-05-15 00:58:39 +0200

    Add initial files

---
+++ src/Lynx.AzureBotApi/AdapterWithErrorHandler.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Builder.TraceExtensions;
+using Microsoft.Bot.Connector.Authentication;
+
+namespace Lynx.TeamsApi;
+
+public class AdapterWithErrorHandler : CloudAdapter
+{
+    public AdapterWithErrorHandler(BotFrameworkAuthentication auth, ILogger<IBotFrameworkHttpAdapter> logger, ConversationState? conversationState = default)
+        : base(auth, logger)
+    {
+        OnTurnError = async (turnContext, exception) =>
+        {
+            // Log any leaked exception from the application.
+            // NOTE: In production environment, you should consider logging this to
+            // Azure Application Insights. Visit https://aka.ms/bottelemetry to see how
+            // to add telemetry capture to your bot.
+            logger.LogError(exception, "[OnTurnError] unhandled error : {errorMessage}", exception.Message);
+
+            // Send a message to the user
+            await turnContext.SendActivityAsync("The bot encountered an error or bug.");
+            await turnContext.SendActivityAsync("To continue to run this bot, please fix the bot source code.");
+
+            if (conversationState is not null)
+            {
+                try
+                {
+                    // Delete the conversationState for the current conversation to prevent the
+                    // bot from getting stuck in a error-loop caused by being in a bad state.
+                    // ConversationState should be thought of as similar to "cookie-state" in a Web pages.
+                    await conversationState.DeleteAsync(turnContext);
+                }
+                catch (Exception e)
+                {
+                    logger.LogError(e, "Exception caught on attempting to Delete ConversationState : {errorMessage}", e.Message);
+                }
+            }
+
+            // Send a trace activity, which will be displayed in the Bot Framework Emulator
+            await turnContext.TraceActivityAsync("OnTurnError Trace", exception.Message, "https://www.botframework.com/schemas/error", "TurnError");
+        };
+    }
+}
---
+++ src/Lynx.AzureBotApi/HostedServices/WriterHostedService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Schema;
+using System.Collections.Concurrent;
+using System.Threading.Channels;
+
+namespace Lynx.TeamsApi.HostedServices;
+
+internal class WriterHostedService : BackgroundService
+{
+    private readonly ChannelReader<string> _reader;
+    private readonly ILogger _logger;
+    private readonly ConcurrentDictionary<string, ConversationReference> _conversationReferences;
+    private readonly IBotFrameworkHttpAdapter _adapter;
+    private readonly string _appId;
+
+    public WriterHostedService(
+        ChannelReader<string> reader,
+        ConcurrentDictionary<string, ConversationReference> conversationReferences,
+        IBotFrameworkHttpAdapter adapter,
+        IConfiguration configuration,
+        ILogger<WriterHostedService> logger)
+    {
+        _reader = reader;
+        _conversationReferences = conversationReferences;
+        _adapter = adapter;
+        _appId = configuration["MicrosoftAppId"] ?? string.Empty;
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        try
+        {
+            await foreach (var output in _reader.ReadAllAsync(stoppingToken))
+            {
+                foreach (var conversationReference in _conversationReferences.Values)
+                {
+                    await ((BotAdapter)_adapter).ContinueConversationAsync(_appId, conversationReference,
+                        async (context, token) => await BotCallback(output, context, token), stoppingToken);
+                }
+
+                //_logger.Debug($"[Lynx]\t{output}");
+                Console.WriteLine(output);
+            }
+        }
+        catch (Exception e)
+        {
+            _logger.LogCritical(e, "Exception in {className}", nameof(WriterHostedService));
+        }
+        finally
+        {
+            _logger.LogInformation($"Finishing {nameof(WriterHostedService)}");
+        }
+    }
+
+    private static async Task BotCallback(string message, ITurnContext turnContext, CancellationToken cancellationToken)
+    {
+        await turnContext.SendActivityAsync(message, cancellationToken: cancellationToken);
+    }
+}
